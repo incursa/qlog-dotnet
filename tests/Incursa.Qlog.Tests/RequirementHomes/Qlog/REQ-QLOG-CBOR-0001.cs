8d5e9b2955e204b73ed8e70208c511d2d1b97b80
Samuel McAravey <samuel@mcaravey.pro>
2026-05-28 14:47:10 -0600

    Enforce Apache copyright headers

--- tests/Incursa.Qlog.Tests/RequirementHomes/Qlog/REQ-QLOG-CBOR-0001.cs
+++ tests/Incursa.Qlog.Tests/RequirementHomes/Qlog/REQ-QLOG-CBOR-0001.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 Incursa LLC.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
 using System.Formats.Cbor;
 using System.IO;
 using Incursa.Qlog.Serialization.Cbor;
